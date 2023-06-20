b31643d23b5e594c20aaddf2811949d5b6f44d4a
dnierman0920 <40249436+dnierman0920@users.noreply.github.com>
2023-06-20 11:23:30 -0400

    add create and get buyer route tests

--- src/__tests__/routes/buyer.test.ts
+++ src/__tests__/routes/buyer.test.ts
@@ -1,27 +1,49 @@
 import { DatabaseDependency } from "../../services/databaseDependency";
 import { MongoClient } from "mongodb";
+import { ServerDependency } from "../../services/serverDependency";
+import request from "supertest";
+import { BuyerController } from "../../controllers/buyer.controller";
 
 describe("buyerRoutes-test", function () {
-  let connection: MongoClient;
   let dbd: DatabaseDependency;
+  let server: ServerDependency;
+  let buyerController: BuyerController;
+  const buyer = {
+    name: "John Doe",
+  };
 
   beforeAll(async () => {
     dbd = new DatabaseDependency(
       "mongodb://localhost:27017",
       "reverseAuctionDBTest"
     );
-    connection = await dbd.connectToDatabase();
-    await dbd.db.dropDatabase();
+    await dbd.connectToDatabase();
+    server = new ServerDependency(5050);
+    buyerController = new BuyerController();
   });
 
   afterAll(async () => {
-    await connection.close();
+    await dbd.client.close();
+    server.server.close();
   });
 
-  describe("cleanDB-test", function () {
-    it("is the db clean?", async function () {
-      const collections = (await dbd.db.collections()).length;
-      expect(collections).toBe(0);
-    });
+  afterEach(async () => {
+    await dbd.db.dropDatabase();
+  });
+
+  it("create buyer", async function () {
+    const res = await request(server.app).post("/buyer").send(buyer);
+    expect(res.statusCode).toEqual(201);
+    const body = res.text.replace(/[\\"]/g, "");
+    expect(body).toBe(
+      `Successfully created a new buyer with the name ${buyer.name}`
+    );
+  });
+  it("get buyer", async function () {
+    await buyerController.createBuyer(buyer);
+    const res = await request(server.app).get("/buyer");
+    expect(res.statusCode).toEqual(200);
+    const name = res.body[0].name;
+    expect(buyer.name).toBe(name);
   });
 });
